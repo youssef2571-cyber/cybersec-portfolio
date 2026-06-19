d17949925a61355fd0ed0987b9ac0dddbe2db238
youssef2571-cyber <youssefelhadeg2002@gmail.com>
2026-06-20 00:40:57 +0100

    Update mini_siem.py

--- SOC_SIEM/mini_siem.py
+++ SOC_SIEM/mini_siem.py
@@ -25,7 +25,6 @@ def parse_and_correlate(log_line):
     
     # ---------------------------------------------------------
     # PARSER 1: Custom IDS/IPS Alerts
-    # Example: "[IDS ALERT] Port Scan detected from 192.168.1.105"
     # ---------------------------------------------------------
     ids_pattern = r"\[IDS ALERT\].*from (\d+\.\d+\.\d+\.\d+)"
     ids_match = re.search(ids_pattern, log_line)
@@ -48,13 +47,12 @@ def parse_and_correlate(log_line):
         # Clean old logs and check threshold
         failed_auth_tracker[attacker_ip] = [t for t in failed_auth_tracker[attacker_ip] if current_time - t <= TIME_WINDOW]
         if len(failed_auth_tracker[attacker_ip]) >= 5:
-            trigger_alert("🔥 HIGH", "SSH Brute Force Attack", attacker_ip, "5+ failed logins in 60s")
+            trigger_alert("HIGH", "SSH Brute Force Attack", attacker_ip, "5+ failed logins in 60s")
             failed_auth_tracker[attacker_ip] = [] # Reset
         return
 
     # ---------------------------------------------------------
     # PARSER 3: SSH Successful Logins (Cross-Correlation Rule)
-    # Example: "Accepted password for root from 192.168.1.105"
     # ---------------------------------------------------------
     ssh_success_pattern = r"Accepted password for .* from (\d+\.\d+\.\d+\.\d+)"
     success_match = re.search(ssh_success_pattern, log_line)
@@ -65,14 +63,14 @@ def parse_and_correlate(log_line):
         # CROSS-CORRELATION: Did this IP trigger an IDS alert recently?
         if attacker_ip in recent_ids_alerts:
             time_diff = current_time - recent_ids_alerts[attacker_ip]
-            if time_diff <= 300: # Within 5 minutes
+            if time_diff <= 300: 
                 trigger_alert(
-                    "💀 CRITICAL", 
+                    " CRITICAL", 
                     "COMPROMISED HOST (Lateral Movement)", 
                     attacker_ip, 
                     f"Successful SSH login occurred {int(time_diff)}s after a Network IDS Alert!"
                 )
-                del recent_ids_alerts[attacker_ip] # Reset
+                del recent_ids_alerts[attacker_ip] 
         return
 
 def follow_log(filename):
@@ -91,8 +89,8 @@ def follow_log(filename):
 
 if __name__ == "__main__":
     print("==================================================")
-    print(" 👁️  Advanced SOC Correlation Engine")
-    print(" 🛡️  Active Rules:")
+    print("  Advanced SOC Correlation Engine")
+    print(" Active Rules:")
     print("     1. SSH Brute Force Detection")
     print("     2. IDS Alert + SSH Success (Critical Compromise)")
     print("==================================================\n")
